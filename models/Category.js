2fc0f0562525ec52a8421e1da710a450e7eda278
cmarvann <covingtonmarvann@gmail.com>
2022-08-01 23:03:19 -0400

    authenticate the login route

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,13 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Category extends Model {}
+// Create Category model
+class Category extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 Category.init(
   {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,23 +43,51 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+ // create a new category
+   // expects {categoryname: 'backendit', email: 'backendit@gmail.com', password: 'password123456'}
+   Category.create({
+     categoryname: req.body.categoryname,
+     email: req.body.email,
+     password: req.body.password
+   })
+     .then(dbCategoryData => res.json(dbCategoryData))
+     .catch(err => {
+       console.log(err);
+       res.status(500).json(err);
+     });
+ });
+
+router.post('/login', (req, res) => {
+   // login - Query operation
   // create a new category
     // expects {categoryname: 'backendit', email: 'backendit@gmail.com', password: 'password123456'}
-    Category.create({
-      categoryname: req.body.categoryname,
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(dbCategoryData => res.json(dbCategoryData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-  
+    Category.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(400).json({ message: 'No category with that email address!' });
+      return;
+    }
+
+    res.json({ category: dbCategoryData });
+
+    // Verify category
+    const validPassword = dbCategoryData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ category: dbCategoryData, message: 'You are now logged in!' });
+    
+  }); 
+});
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+    // expects {categoryname: 'backendit', email: 'backendit@gmail.com', password: 'password123456'}
   Category.update(req.body, {
     individualHooks: true,
     where: {
